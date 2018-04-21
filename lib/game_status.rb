cf486d3983ee61b91107799fcb69401bc11cf3c9
Daniel Doughty <djdoughty@gmail.com>
2018-04-21 08:51:54 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,4 +4,47 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7],[2,5,8],[0,4,8],[2,4]]
+WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combo|
+    if board[combo[0]] == "X" && board[combo[1]] =="X" && board[combo[2]] == "X"
+      return [combo[0], combo[1], combo[2]]
+    elsif board[combo[0]] == "O" && board[combo[1]] =="O" && board[combo[2]] == "O"
+      return [combo[0], combo[1], combo[2]]
+    end
+  end
+  return false
+end
+
+def full?(board)
+  index = 0
+    8.times do 
+      if !(position_taken?(board, index))
+        return false
+      end
+    index += 1
+    end
+    return true
+end
+
+def draw? (board)
+  if !won?(board) && full?(board)
+    return true
+  end
+  return false
+end
+
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+    return true
+  end
+  return false
+end
+
+def winner(board)
+  puts board[won?(board)[0]]
+end
+
+board = [" ","X","X"," ","X"," ","X","X","X"]
+winner(board)
